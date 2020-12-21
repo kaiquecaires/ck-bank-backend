650e1894ba6a09d1a79c18b619849fb8bef87154
kaique coimbra <kaique.caires@yahoo.com>
2020-12-21 15:09:12 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,11 @@ import 'express-async-errors';
 import './typeorm';
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
