f9690dd08355bb0769d66ebe430c1a0d2b0ba5cf
kaique coimbra <kaique.caires@yahoo.com>
2020-12-25 07:50:50 -0300

    test: ensure should be able create a session

--- src/services/tests/CreateSessionService.spec.ts
+++ src/services/tests/CreateSessionService.spec.ts
@@ -1,1 +1,48 @@
-describe('CreateSession', () => {});
+import { IHashProvider } from '../../providers/HashProvider/models/IHashProvider';
+import { ITokenProvider } from '../../providers/TokenProvider/models/ITokenProvider';
+import { IUserRepository } from '../../repositories/IUserRepository';
+import { FakeUserRepository } from '../../typeorm/repositories/fakes/FakeUserRepository';
+import { TokenProvider } from '../../providers/TokenProvider/Fakes/FakeTokenProvider';
+import { FakeHashProvider } from '../../providers/HashProvider/Fakes/FakeHashProvider';
+import { CreateSessionService } from '../implementations/CreateSessionService';
+import { CreateUserService } from '../implementations/CreateUserService';
+
+describe('CreateSession', () => {
+  let createSessionService: CreateSessionService;
+  let userRepository: IUserRepository;
+  let tokenProvider: ITokenProvider;
+  let hashProvider: IHashProvider;
+  let createUserService: CreateUserService;
+
+  beforeEach(() => {
+    userRepository = new FakeUserRepository();
+    tokenProvider = new TokenProvider();
+    hashProvider = new FakeHashProvider();
+    createUserService = new CreateUserService(
+      userRepository,
+      hashProvider
+    );
+    createSessionService = new CreateSessionService(
+      userRepository,
+      tokenProvider,
+      hashProvider
+    );
+  });
+
+  it('should be able create a session', async () => {
+    const user = {
+      email: 'teste@teste.com',
+      name: 'kaique caires',
+      password: '123456'
+    }
+
+    await createUserService.execute(user);
+
+    const response = await createSessionService.execute({
+      email: 'teste@teste.com',
+      password: '123456'
+    });
+
+    expect(response.user).toHaveProperty('id');
+  });
+});
