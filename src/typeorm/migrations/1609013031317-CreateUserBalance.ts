d4925a9f06dc274de28df83912072275f74b053d
kaique coimbra <kaique.caires@yahoo.com>
2020-12-26 17:22:55 -0300

    feat: implements a new column balance in user table

--- src/typeorm/migrations/1609013031317-CreateUserBalance.ts
+++ src/typeorm/migrations/1609013031317-CreateUserBalance.ts
@@ -1,11 +1,16 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
 
 export class CreateUserBalance1609013031317 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn('users', new TableColumn({
+      name: 'balance',
+      type: 'decimal',
+      default: 0.00
+    }));
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('users', 'balance');
+  }
 }
