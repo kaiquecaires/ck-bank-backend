d913d4612e47a4845a4ad5ad7e3e33b493f085b2
kaique coimbra <kaique.caires@yahoo.com>
2021-01-13 07:26:39 -0300

    test: ensure SignUpController return 400 if no email is provided

--- src/presentation/controllers/signup.ts/signup.spec.ts
+++ src/presentation/controllers/signup.ts/signup.spec.ts
@@ -22,4 +22,20 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error())
   })
+
+  test('should return 400 if no email is provided', async () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error())
+  })
 })
