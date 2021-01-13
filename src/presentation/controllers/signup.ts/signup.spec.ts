2f889d39d4c1a823bd90517b0a6c8b473f4c0d14
kaique coimbra <kaique.caires@yahoo.com>
2021-01-13 07:29:16 -0300

    test: ensure SignUpController return 400 if no password is provded

--- src/presentation/controllers/signup.ts/signup.spec.ts
+++ src/presentation/controllers/signup.ts/signup.spec.ts
@@ -38,4 +38,20 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error())
   })
+
+  test('should return 400 if no password is provided', async () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error())
+  })
 })
